a5e2b1899dad9e8d76d4829108f3643467d3371a
Tom Smyth <tom@sassafras.coop>
2015-01-15 09:31:01 -0500

    Moved nav bar

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,16 @@
 <aside id="sidebar" role="complementary">
-<?php if ( is_active_sidebar( 'primary-widget-area' ) ) : ?>
-<div id="primary" class="widget-area">
-<ul class="xoxo">
-<?php dynamic_sidebar( 'primary-widget-area' ); ?>
-</ul>
-</div>
-<?php endif; ?>
+  <nav id="menu" role="navigation">
+    <div id="search">
+      <?php get_search_form(); ?>
+    </div>
+    <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+  </nav>
+
+  <?php if ( is_active_sidebar( 'primary-widget-area' ) ) : ?>
+    <div id="primary" class="widget-area">
+      <ul class="xoxo">
+        <?php dynamic_sidebar( 'primary-widget-area' ); ?>
+      </ul>
+    </div>
+  <?php endif; ?>
 </aside>
