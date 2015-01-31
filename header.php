6893b3fedcb0e2fd6a139f7166abb4defead7d05
Tom Smyth <tom@sassafras.coop>
2015-01-30 20:56:55 -0500

    Added favicon

--- header.php
+++ header.php
@@ -4,7 +4,14 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width" />
     <title><?php wp_title( ' | ', true, 'right' ); ?></title>
+
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+
+    <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicon/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicon/favicon-16x16.png" sizes="16x16">
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon/favicon.ico">
+
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
