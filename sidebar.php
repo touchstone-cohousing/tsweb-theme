c8d3d72c0b88e3e43d9adef7bb1e7b4467e69e4e
Tom Smyth <tom@sassafras.coop>
2015-02-01 09:44:34 -0500

    Menu and style tweaks

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,11 @@
     <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
 
     <div class="menu-title">Members Only</div>
-    <?php wp_nav_menu( array( 'theme_location' => 'extra-menu' ) ); ?>
+    <?php if (is_user_logged_in()) { ?>
+      <?php wp_nav_menu( array( 'theme_location' => 'extra-menu' ) ); ?>
+    <?php } else { ?>
+      <a href="/wp-login.php?redirect_to=%2F">Login</a>
+    <?php } ?>
   </nav>
 
   <?php if ( is_active_sidebar( 'primary-widget-area' ) ) : ?>
